2f9108c0cad67f045b2e281bfba3f4551e09a136
mxsyx <zsimline@163.com>
2020-12-06 16:41:48 +0800

    some useful scripts

---
+++ m3u8-downloader/main.ts
@@ -0,0 +1,51 @@
+#! /usr/bin/env node
+// H.264 Downloader
+
+const THREADES_NUMBER = 10
+const OUTPUT_DIR = Date.now()
+
+if (OUTPUT_DIR) {
+  Deno.mkdirSync(OUTPUT_DIR.toString())
+}
+
+const baseUrl = Deno.args[0].replace("index.m3u8", "1000k/hls")
+let lock = false
+
+interface TS { id: string, url: string }
+
+const tses: TS[] = []
+const concats: string[] = []
+
+async function download(rid: number) {
+  while (true) {
+    const ts = tses.shift()
+    if (!ts) {
+      if (!lock) {
+        const encoder = new TextEncoder()
+        Deno.writeFileSync(`./${OUTPUT_DIR}/concat.txt`, encoder.encode(concats.sort().join('\n')))
+        lock = true
+      }
+      return
+    }
+    console.log(`下载器: ${rid}  下载: ${ts.url}  剩余: ${tses.length}  `)
+    const response = await fetch(ts.url)
+    const arrayBuffer = await response.arrayBuffer()
+    Deno.writeFileSync(`./${OUTPUT_DIR}/${ts.id}`, new Uint8Array(arrayBuffer))
+    concats.push(`file '${ts.id}'`)
+  }
+}
+
+fetch(`${baseUrl}/index.m3u8`)
+  .then((response) => response.text())
+  .then((content) => {
+    for (let match of content.matchAll(/.*\.ts/g)) {
+      tses.push({ id: match[0], url: `${baseUrl}/${match[0]}` })
+    }
+
+    const len = parseInt(String(tses.length / THREADES_NUMBER))
+    for (let i = 0; i * len < tses.length; ++i) {
+      download(i + 1)
+    }
+  })
+
+export default {}
---
+++ v2ray-switch/main.js
@@ -0,0 +1,67 @@
+#! /usr/bin/env node
+// v2ray-switch
+
+const net = require('net');
+const https = require("https")
+const fs = require('fs')
+const { spawnSync } = require('child_process')
+
+const CONFIG_PATH = '/usr/local/etc/v2ray/config.json'
+const SUBSCRIPTION_URL = ''
+
+let locked = false
+
+function parseSubscription(data) {
+  const buffer = Buffer.from(data, 'base64')
+  const b64List = buffer.toString().replace(/vmess:\/\//g, '').split('\n').slice(0, -1)
+  return b64List.map(b => JSON.parse(Buffer.from(b, 'base64').toString()))
+}
+
+function tcpPing(host, port) {
+  const justNow = Date.now()
+  const socket = new net.Socket()
+  socket.setTimeout(5000)
+  return new Promise((resolve, reject) => {
+    socket.on('connect', () => {
+      resolve(Date.now() - justNow)
+      socket.destroy();
+    }).on('error', e => {
+      reject(e.message)
+    }).on('timeout', () => {
+      reject('timeout')
+    }).connect(port, host);
+  })
+}
+
+function main(data) {
+  const vmessList = parseSubscription(data)
+  vmessList.forEach(async vmess => {
+    try {
+      vmess['differ'] = await tcpPing(vmess['add'], vmess['port'])
+      if (!locked) {
+        console.log(vmess)
+        const f = fs.readFileSync(CONFIG_PATH)
+        const config = JSON.parse(f.toString())
+        config['outbounds'][0]['settings']['vnext'][0]['address'] = vmess['add']
+        config['outbounds'][0]['settings']['vnext'][0]['port'] = vmess['port']
+        fs.writeFileSync(CONFIG_PATH, JSON.stringify(config))
+        spawnSync('sudo service v2ray restart')
+        locked = true
+      } else {
+        process.kill(process.pid, 'SIGTERM')
+      }
+    } catch (error) {
+      console.log(`${vmess['add']}:${vmess['port']}  ${error}`);
+    }
+  })
+}
+
+// start
+https.get(SUBSCRIPTION_URL, (res) => {
+  let data = ''
+  res.on('data', chunk => data += chunk)
+  res.on("end", () => main(data))
+  res.resume();
+}).on('error', e => {
+  console.log(`Got error: ${e.message}`)
+})
